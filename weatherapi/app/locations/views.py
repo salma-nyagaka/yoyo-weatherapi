4e27f5538c61e4fb161d3aa1afc77013f0d29fa9
salma-nyagaka <salmanyagaka@gmail.com>
2021-09-07 21:17:41 +0300

    update validations

--- weatherapi/app/locations/views.py
+++ weatherapi/app/locations/views.py
@@ -8,10 +8,12 @@ from django.conf import settings
 from .helpers.constants import SUCCESS_MESSAGE, FORBIDDEN_MESSAGE
 from .helpers.validate_params import validate_params
 from .helpers.perform_computations import perform_computations
+from ...helpers.renderers import RequestJSONRenderer
 
 
 class WeatherDataRetrieveApiView(generics.RetrieveAPIView):
     """ Class to fetch weather data"""
+    renderer_classes = (RequestJSONRenderer,)
 
     def get(self, request, city_name):
         """ Function to fetch weather data from
@@ -21,7 +23,7 @@ class WeatherDataRetrieveApiView(generics.RetrieveAPIView):
         params = request.query_params
         key = settings.API_KEY
 
-        if key is not None:
+        if key:
             # Convert QueryDict to Python Dict
             params_dict = params.dict()
 
@@ -34,22 +36,27 @@ class WeatherDataRetrieveApiView(generics.RetrieveAPIView):
             data = requests.get(url, params=params_dict)
             response_data = json.loads(data.content)
 
-            # Get the computed data
-            maximum, minimum, average, median = perform_computations(
-                response_data)
+            if data.status_code == 200:
 
-            computation_data = {
-                "maximum": maximum,
-                "minimum": minimum,
-                "average": average,
-                "median": median
-            }
+                # Get the computed data
+                maximum, minimum, average, median = perform_computations(
+                    response_data)
 
+                computation_data = {
+                    "maximum": maximum,
+                    "minimum": minimum,
+                    "average": average,
+                    "median": median
+                }
+                return_message = {
+                    "message": SUCCESS_MESSAGE.format("Weather data fetched"),
+                    "data": computation_data
+                }
+                return Response(return_message, status=status.HTTP_200_OK)
             return_message = {
-                "message": SUCCESS_MESSAGE.format("Weather data fetched"),
-                "data": computation_data
+                "message": response_data['error']['message']
             }
-            return Response(return_message, status=status.HTTP_200_OK)
+            return Response(return_message, status=status.HTTP_400_BAD_REQUEST)
         return_message = {
             "message": FORBIDDEN_MESSAGE
         }
