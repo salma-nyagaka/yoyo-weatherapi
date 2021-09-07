c7d7cfbccc7a8daa54b8388aaaffb23b699fcb2d
salma-nyagaka <salmanyagaka@gmail.com>
2021-09-07 21:39:37 +0300

    update pep8 issues

--- weatherapi/app/locations/helpers/constants.py
+++ weatherapi/app/locations/helpers/constants.py
@@ -1,3 +1,3 @@
 SUCCESS_MESSAGE = '{} successfully'
 FORBIDDEN_MESSAGE = 'You must be authenticated to'  \
-     'access this resource. Kindly provide the Api-Key'
+     ' access this resource. Kindly provide the Api-Key'
